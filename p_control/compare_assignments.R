dc806b530a4ce216267a19a742d5eae13802f0bf
richardjtelford <richard.telford@uib.no>
2023-12-05 09:51:19 +0100

    work for all assignments

--- p_control/compare_assignments.R
+++ p_control/compare_assignments.R
@@ -8,32 +8,69 @@ library(textreuse)
 source_dir <- "p_control/krill"
 dest_dir <- "p_control/krill/txt/"
 
-# get list of files
-file_list <- list.files(path = here(source_dir), full.names = TRUE, pattern = "html")
-file_list
+extract_qmd <- function(source_dir, dest_ext = "txt") {
 
-fs::dir_create(dest_dir)
-file_list |>
-  walk(\(f){
-    print(f)
-  # 1 extract quarto from html
-  read_html(f) |>
-    html_elements(xpath = '//*[@id="quarto-embedded-source-code-modal"]') |> #"div.modal") |>
-    html_text2() |>
-    # save as txt
-    write_lines(file.path(dest_dir, paste0(basename(f), ".txt")))
-})
+  # get list of files
+  file_list <- list.files(path = here(source_dir), full.names = TRUE, pattern = "html")
 
-# compare assigments
-txt_comp <- TextReuseCorpus(dir = dest_dir)
-skipped(txt_comp)
-comparisons <- pairwise_compare(txt_comp, jaccard_similarity, progress = FALSE)
-pc <- pairwise_candidates(comparisons)
+  dest_dir <- file.path(source_dir, dest_ext)
+  fs::dir_create(dest_dir)
+  file_list |>
+    walk(\(f){
+      print(f)
+    # 1 extract quarto from html
+    read_html(f) |>
+      html_elements(xpath = '//*[@id="quarto-embedded-source-code-modal"]') |> #"div.modal") |>
+      html_text2() |>
+      # save as txt
+      write_lines(file.path(dest_dir, paste0(basename(f), ".txt")))
+  })
+}
+
+
+assignments <- c("p_control/salmon", "p_control/otters", "p_control/krill", "p_control/kjottmeis", "p_control/otoliths") |>
+  set_names(basename)
+"p_control/lice"
+assignments |> map(extract_qmd)
+
+
+# compare assignments
+corpora <- assignments |>
+  file.path("txt") |>
+  set_names(names(assignments)) |>
+  map(\(d)TextReuseCorpus(dir = d))
+
+corpora |> map(skipped)
+
+
+comparisons <- corpora |>
+  map(\(crp)pairwise_compare(crp, jaccard_similarity))
+
+
+pc <- comparisons |>
+  map(pairwise_candidates) |>
+  list_rbind(names_to = "assignment")
 
 # plot
 pc |>
-  arrange(-score)
+  group_by(assignment) |>
+  mutate(across(c(a, b), str_remove, ".html")) |>
+  arrange(-score) |>
+  slice(1:10) |>
+  View()
+
 ggplot(pc, aes(x = score)) +
-  geom_histogram()
+  geom_histogram() +
+  geom_rug() +
+  facet_wrap(vars(assignment))
 
 
+comparisons |>
+  map(t) |>
+  map(\(x){
+    colnames(x) <- str_remove(colnames(x), "_.*")
+    rownames(x) <- str_remove(rownames(x), "_.*")
+    x}) |>
+  map(as.dist) |>
+  map(\(d) hclust(1 - d, method = "single")) |>
+  imap(\(h, n){x11(); plot(h, main = n)})
